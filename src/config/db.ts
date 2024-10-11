cc86a6d6daa4eb74cbe7a82b36de3393be6652a0
[Node1] 백기준 <rlwns1580@naver.com>
2024-10-11 10:12:16 +0900

    refactor: .env 파일에 db 설정

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,14 +2,16 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../entity/user.entity";
 import { Task } from "../entity/task.entity";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "127.0.0.1",
-    port: 3306,
-    username: "root",
-    password: "1234",
-    database: "TaskUp",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: false, // 개발 환경에서는 true, 프로덕션에서는 false로 설정
     logging: false,
     entities: [User, Task],

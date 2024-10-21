34708ed2bd9c508b88124735433dc6905ad9a555
[Node1] 백기준 <rlwns1580@naver.com>
2024-10-21 22:54:16 +0900

    코드 리뷰를 위해 머지 생성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -51,8 +51,8 @@ export const userController = {
             res.cookie('token', token, {
                 httpOnly: true, // JavaScript에서 쿠키 접근 불가
                 //secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송, development: 개발 환경 production: 배포 환경 test: 테스트 환경
-                sameSite: 'none', // 같은 사이트 내에서만 쿠키 전송
-                secure: true,
+                sameSite: 'lax', // 같은 사이트 내에서만 쿠키 전송
+                secure: false,
                 maxAge: 60 * 60 * 1000,  // 1시간 동안 유효
             });
             res.status(200).send({message:'로그인 성공' , userinfo: {token}, user:{email, name}});
